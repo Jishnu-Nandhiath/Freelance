5741f7eddf9e638a26135d722f8d163d4d625acd
Jishnu-Nandhiath <jishnun789@gmail.com>
2022-08-12 02:33:49 +0530

    Completed Sign-Up Page

---
+++ main.js
@@ -0,0 +1,12 @@
+let myWindow;
+
+function openWin() {
+    myWindow = window.open("sign-up.html", "", "width=375px, height=812px");
+  }
+  
+
+// It's hard to resize the opening window in Chrome. Only Pop up window Size can be controlled. 
+
+// function resizeWin() {
+//     window.resizeTo(300, 300);
+//   }
